636dfc868fa4f590926f7e4da1abf7a4abebcbe4
mitch344 <35354833+mitch344@users.noreply.github.com>
2023-12-02 14:46:36 -0600

    Refactor & Modularization
    Refactored most of the code & simplified documentation.

--- project_code.py
+++ project_code.py
@@ -1,151 +1,107 @@
-
 import os
 from tkinter import *
 from collections import defaultdict
 
+class RestaurantSearch:
+    def __init__(self):
+        # Initializes the RestaurantSearch class, creating the inverted index and document list
+        self.inverted_index = defaultdict(list)
+        self.documents = []
+        self.load_and_index_files()
 
-
-
-class Search:
-
-    def __init__(self):# this function reads and indexes files
-
-
-        self.invertedindex = invertedindex = defaultdict(list)
-        self.list1 = list1 = []
-        self.titlelist = titlelist= []
-
-        #resturant folder in the same path as the script
-        self.filelocations = os.path.join(os.path.dirname(__file__), "restaurants")
-        if not os.path.exists(self.filelocations):
+    def load_and_index_files(self):
+        # Loads restaurant data from files and indexes them for search
+        file_location = os.path.join(os.path.dirname(__file__), "restaurants")
+        if not os.path.exists(file_location):
             messagebox.showerror("Error", "Restaurant folder does not exist")
             exit()
 
-        os.chdir(self.filelocations) #make the program read files from the specified path
+        for filename in os.listdir(file_location):
+            with open(os.path.join(file_location, filename), "r") as file:
+                content = file.read()
+                self.documents.append(content)
+                self.index_document(content)
 
-        for eachfile in os.listdir(): #for every file in the folder
+    def index_document(self, content):
+        # Creates an index of words from the document content for search functionality
+        tokens = content.lower().split()
+        doc_id = len(self.documents) - 1
+        for token in tokens:
+            cleaned_token = token.strip(".,").replace("and", "").replace("the", "")
+            if doc_id not in self.inverted_index[cleaned_token]:
+                self.inverted_index[cleaned_token].append(doc_id)
 
-            with open(eachfile, "r") as r: #open the file
-                self.list1.append(r.read()) #append the contents of the file to list1
+    def search(self, query, location):
+        # Searches indexed documents based on the query and location, returning relevant results
+        query_terms = query.lower().split()
+        query_len = len(query_terms)
+        result_list = []
+        for term in query_terms:
+            cleaned_term = term.strip(".,").replace("and", "").replace("the", "")
+            result_list.extend(self.inverted_index[cleaned_term])
 
-                    
-        for eachdoccontent in list1: #for each batch of text in a file
-            tokens = eachdoccontent.split() #split all text into tokens
+        rank_dict = {doc_id: result_list.count(doc_id) / query_len for doc_id in set(result_list)}
+        ranked_results = sorted(rank_dict, key=rank_dict.get, reverse=True)
 
-            for eachtoken in tokens: #for each token in a file
+        return self.format_results(ranked_results, rank_dict, location), len(ranked_results)
 
-                lowercasewords = eachtoken.lower() #lowercase each token before indexing
-                removed_1 = lowercasewords.replace(".","") #remove "." before indexing
-                removed_2 = removed_1.replace(",", "")  # remove "," before indexing
-                removed_3 = removed_2.replace("and", "")  # remove "and" before indexing
-                removed_4 = removed_3.replace("the", "")  # remove "the" before indexing
-
-                if list1.index(eachdoccontent) not in invertedindex[removed_4]: #if a document number is not a value for a particular token
-                    invertedindex[removed_4].append(list1.index(eachdoccontent)) #add that document number as a value for that token
-
-
-
-    def searchindex(self,query,location): #this function queries the inverted index
-
-        querylen = len(query.split())
-        resultlist = []
-        for eachqueryterm in query.split():#split all query terms
-
-            lowercasequery = eachqueryterm.lower()#lowercase the user query
-            query_remove_1 = lowercasequery.replace(".","")#remove "." from user query
-            query_remove_2 = query_remove_1.replace(",", "") #remove "," from user query
-            if eachqueryterm == "and": #remove "and" from user query
-                querylen = querylen - 1
-            if eachqueryterm == "the": #remove "the" from user query
-                querylen = querylen - 1
-
-            for item in self.invertedindex[query_remove_2]: #for every item in the inverted index for a query term
-                resultlist.append(item) #append each item to result list
-
-        rankdict = {}
-        for item in resultlist:    #for each item in the result list
-            if item in rankdict:    #if the item is in the rankdict
-                rankdict[item] = rankdict[item] + 1 #increase the count by 1
-            else:
-                rankdict[item] = 1 #if not, then assign 1
-
-        for item in rankdict:
-            rankdict[item] = rankdict[item] / querylen #calculating the relavance score
-
-        ranklist = (sorted(rankdict, key=rankdict.get, reverse = True))#this sorts the dictionary by value
+    def format_results(self, ranked_results, rank_dict, location):
+        # Formats the search results for display, filtering by location if specified
+        output = []
+        location_match_count = 0
+        for doc_id in ranked_results:
+            title, loc = self.documents[doc_id].split("\n")[:2]
+            if location and loc != location:
+                continue
+            location_match_count += 1
+            relevance = round(rank_dict[doc_id] * 100, 3)
+            output.append(f"{title}, LOC: {loc}, relevance: {relevance}%")
+        return output, location_match_count
 
 
-        totalcounter = 1
-        relloccounter = 0
-        guilist = []
-        # this is the output if at least 1 word of the query is in a document
-        for item in ranklist:
-            #print the first line in the file and the relevance score
-            #print(self.list1[item].split("\n")[0],": relevance "+str(round(rankdict[item]*100,3)) + "%")
-            if location != "":
-                if self.list1[item].split("\n")[1] == location :
-                    guilist.append("#"+ str(totalcounter) + " " +self.list1[item].split("\n")[0] + ", LOC: " + self.list1[item].split("\n")[1] + ", relevance: "+str(round(rankdict[item]*100,3)) + "%")
-                    relloccounter = relloccounter + 1
-                else:
-                    guilist.append("Location Filter Applied")
+class GUI:
+    def __init__(self, master):
+        # Initializes the GUI elements and sets up the main window
+        self.master = master
+        master.title('The Restaurant Selector')
+        master.geometry("850x600")
 
-            if location == "":
-                guilist.append("#"+ str(totalcounter) + " "  +self.list1[item].split("\n")[0] + ", LOC: " + self.list1[item].split("\n")[1] + ", relevance: " + str(round(rankdict[item] * 100, 3)) + "%")
-            totalcounter = totalcounter + 1
-        # this is the output if no words of the query are in any documents
-        if len(ranklist) == 0:
-            #print("No words in any document matched your query.")
+        self.search_engine = RestaurantSearch()
 
-            guilist.append("No words in any document matched your query.")
+        Label(master, text="What do you feel like eating today?", font=("Times", 21)).pack()
+        self.query_entry = Text(master, width=40, height=5)
+        self.query_entry.pack()
 
+        Label(master, text="(Optional) Please enter a city name and the first two letters of the state. e.g., Urbana IL", font=("Times", 8)).pack()
+        self.location_entry = Entry(master, width=20)
+        self.location_entry.pack()
 
-        return guilist, totalcounter-1, relloccounter
+        self.results_text = Text(master, width=95, height=24)
+        self.results_text.pack()
 
+        Button(master, text="Find Restaurants", command=self.perform_search).pack()
 
-gui = Tk()
-gui.geometry("850x600")
-gui.title('The Restaurant Selector')
+    def perform_search(self):
+        # Initiates a search when the 'Find Restaurants' button is clicked
+        query = self.query_entry.get("1.0", "end").strip()
+        location = self.location_entry.get().strip()
+        results, total_results = self.search_engine.search(query, location)
+        results, location_match_count = results
 
-titlelabel = Label(gui, text="What do you feel like eating today?",font=("Times", 21))
-titlelabel.pack()
+        self.results_text.delete("1.0", END)
+        self.results_text.insert("1.0", f"{total_results} total results returned\n")
+        if location:
+            self.results_text.insert(END, f"Location Filter Applied\n{location_match_count} relevant to your specified location\n")
+        else:
+            self.results_text.insert(END, "No Location Filter Applied\n")
+        for result in results:
+            self.results_text.insert(END, result + "\n")
 
-inputtxt = Text(gui, width=40,height=5)
-inputtxt.pack()
+def main():
+    # The main function to run the application
+    root = Tk()
+    gui = GUI(root)
+    root.mainloop()
 
-filterlabel = Label(gui, text="(Optional) Please enter a city name and the first two letters of the state. eg. Urbana IL",font=("Times", 8))
-filterlabel.pack()
-
-filterlabel2 = Label(gui, text="NOTE: The format must exactly match the format of the example.",font=("Times", 8))
-filterlabel2.pack()
-
-locationtxt = Entry(gui, width=20)
-locationtxt.pack()
-
-resultslabel = Label(gui, text="Results",font=("Times", 12))
-resultslabel.pack()
-
-results = Text(gui, width=95,height=24)
-results.pack()
-
-
-def guifunction():
-
-    location = locationtxt.get()
-
-    gettext = inputtxt.get("1.0", "end")
-
-    results.insert("1.0", "\nYour Query: "+gettext)
-
-    for item in Search().searchindex(gettext,location)[0].__reversed__():
-        results.insert("1.0", "\n" + str(item))
-
-    results.insert("1.0", "\n" + str(Search().searchindex(gettext, location)[2]) + " relevant to your specified location")
-    results.insert("1.0", "\n" + str(Search().searchindex(gettext, location)[1]) + " total results returned")
-
-
-
-searchbutton = Button(gui, text ="Find Restaurants", command = guifunction)
-searchbutton.place(x=70,y=60)
-
-
-gui.mainloop()
+if __name__ == "__main__":
+    main()
